cf16c596e190b2eb970f0be9e711aa050e097731
Rahul Singh <rahul.s@browserstack.com>
2025-12-22 21:05:03 +0530

    add diverse tests for TRA

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   coverageProvider: "v8",
-  maxConcurrency: 5,
   maxWorkers: 5,
   roots: ["src"],
-  testMatch: ["**/*.test.js"],
+  testMatch: ["**/test.js"],
   testPathIgnorePatterns: ["/node_modules/"],
-  testTimeout: 60 * 1000,
+  testTimeout: 30 * 1000,
 };
